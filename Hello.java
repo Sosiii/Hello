be1235442a55d73bae76c6c1c1a667779aa53cea
Sosiii <89493230+Sosiii@users.noreply.github.com>
2021-08-24 22:10:58 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,16 @@
+/**************************************
+*Hello
+*Sosna Marshet
+*08/24/2021
+*CMSC 255-004
+***************************************/
+public class Hello {
+   public static void main(String[] args){
+   System.out.println("< Sosna Marshet>");
+   System.out.println("< Hello >");
+   System.out.println("< Self introduction >");
+   System.out.println("< 08/24/2021 >");
+   System.out.println("<Introduction to Programming - sec 004 >");
+   System.out.println();
+   }
+}
